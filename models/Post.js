ce6a1a5147346aa5548f4bee6ecbf879262e4373
Rishabh Kumar <ad077162@gmail.com>
2026-07-01 01:12:58 +0530

    Add Post model

---
+++ models/Post.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+    maxlength: 1000,
+  },
+  image: {
+    type: String,
+    default: '',
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment',
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Post', PostSchema);
