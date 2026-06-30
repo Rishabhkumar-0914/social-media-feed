b7777bbee3bdef6d25194b64e44620458862e139
Rishabh Kumar <ad077162@gmail.com>
2026-07-01 01:13:14 +0530

    Add Comment model

---
+++ models/Comment.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const CommentSchema = new mongoose.Schema({
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post',
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+    maxlength: 500,
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Comment', CommentSchema);
